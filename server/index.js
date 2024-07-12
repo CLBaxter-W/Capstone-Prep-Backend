ab1afbcf8dca2ab3c524614aaaefb870453f3133
CLBaxter-W <168162275+CLBaxter-W@users.noreply.github.com>
2024-07-12 11:13:08 -0600

    added register functionality

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const { app } = require("../src/shared/shared");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`I am listening on port number ${PORT}`);
+});
+
+const userRoutes = require("../src/routes/userRoutes");
+app.use("/api/user", userRoutes);
---
+++ src/shared/shared.js
@@ -0,0 +1,25 @@
+
+// Create the backend application layer
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// add the encryption 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// add the router
+const route = express.Router();
+
+// add the db layer wrapper
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// export variables
+module.exports = {
+  app,
+  bcrypt,
+  jwt,
+  route,
+  prisma,
+};
