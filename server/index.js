a633541bbf88e50e1561f8dd399b648e1ff3532e
CLBaxter-W <168162275+CLBaxter-W@users.noreply.github.com>
2024-07-15 16:55:53 -0600

    adding Access-Control-Allow-Origin to index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ const { app } = require("../src/shared/shared");
 
 const PORT = process.env.PORT || 3000;
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`I am listening on port number ${PORT}`);
 });
