fe04148abee02fc673d55d158665e157b2c10eb1
Drew Hastings <drewhastings@Drews-MacBook-Pro.local>
2024-07-15 17:10:33 -0400

    Revert "cors bug fix"
    This reverts commit 4929e5ee2cbf4a68f195e524708eae2385881b14.
    
    reverted to cors without options
    :wq

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
 const { app } = require("../src/shared/shared");
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const corsOptions = {
-  origin: "http://capstone-prep-backend-vjwd.onrender.com/",
-  optionsSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`I am listening on port number ${PORT}`);
