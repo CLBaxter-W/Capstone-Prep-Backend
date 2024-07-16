41fafd7575bfc051d8f0512364d22988be7ad62b
CLBaxter-W <168162275+CLBaxter-W@users.noreply.github.com>
2024-07-15 23:31:00 -0600

    removing specific address

--- server/index.js
+++ server/index.js
@@ -14,16 +14,15 @@ const PORT = process.env.PORT || 3000;
 // });
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type, accept"
+    "X-Requested-With,content-type"
   );
-  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
